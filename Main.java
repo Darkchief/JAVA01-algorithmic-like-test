f51fa1c7536c68f64300ef23042db4d115c5d06c
Bruno <bruno.selmi92@gmail.com>
2023-10-11 19:32:22 +0200

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+      try{
+        Scanner scanner = new Scanner(System.in);
+        String string = "";
+         while(scanner.hasNext()){
+             string = string + scanner.next() + " ";
+         }
+          System.out.print(string.trim());
+//          System.out.println(scanner.next());
+        
+      } catch(Exception e){
+        System.out.println("An error was found ");
+      }
+      
+    }
+}
